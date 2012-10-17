ad52d67fd6f309d0ca68b0050c91e45b4076fea2
Antoine Aubry <aaubry@gmail.com>
2012-10-17 12:16:07 +0100

    Add separate console application.

---
+++ FluentOMatic.Console/Program.cs
@@ -0,0 +1,48 @@
+﻿using FluentOMatic.Emission;
+using FluentOMatic.States;
+using NDesk.Options;
+using System.IO;
+using System.Linq;
+
+namespace FluentOMatic.Console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string inputFile = null;
+			string outputFile = null;
+			string namespaceName = null;
+			bool help = false;
+
+			var options = new OptionSet
+			{
+				{ "i|input=", v => inputFile = v },
+				{ "o|output=", v => outputFile = v },
+ 				{ "ns|namespace=", v => namespaceName = v },
+				{ "h|?|help", v => help = true },
+			};
+
+			options.Parse(args);
+			if (help)
+			{
+				options.WriteOptionDescriptions(System.Console.Out);
+				return;
+			}
+
+			var parser = new Parser(inputFile != null ? new Scanner(inputFile) : new Scanner(System.Console.OpenStandardInput()));
+			parser.Parse();
+
+			var graphBuilder = new StateGraphBuilder();
+			var states = graphBuilder.BuildGraph(parser.Syntax);
+
+			var output = outputFile != null ? File.CreateText(outputFile) : System.Console.Out;
+
+			var generator = new CodeGenerator();
+			generator.GenerateCode(states.First(), output, namespaceName ?? parser.Syntax.Name);
+
+			output.Flush();
+			output.Close();
+		}
+	}
+}
--- FluentOMatic.Console/Properties/AssemblyInfo.cs
+++ FluentOMatic.Console/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("FluentOMatic.Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("FluentOMatic.Console")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1b8203a6-8a51-42f1-8a6e-247a9ea3b7f0")]
+[assembly: Guid("822a87ea-bf3d-4b12-90a8-3c7d4cdbb669")]
 
 // Version information for an assembly consists of the following four values:
 //
