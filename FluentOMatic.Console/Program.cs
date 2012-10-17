be7cb4db429092452d73002a7afee75ef1400344
Antoine Aubry <aaubry@gmail.com>
2012-10-17 12:53:15 +0100

    Add GPL license.

--- FluentOMatic.Console/Program.cs
+++ FluentOMatic.Console/Program.cs
@@ -1,4 +1,21 @@
-﻿using FluentOMatic.Emission;
+﻿//    Copyright 2012 Antoine Aubry
+//    
+//    This file is part of FluentOMatic.
+//
+//    FluentOMatic is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    FluentOMatic is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+using FluentOMatic.Emission;
 using FluentOMatic.States;
 using NDesk.Options;
 using System.IO;
@@ -26,6 +43,26 @@ namespace FluentOMatic.Console
 			options.Parse(args);
 			if (help)
 			{
+				var notice = @"
+					This program is free software: you can redistribute it and/or modify
+					it under the terms of the GNU General Public License as published by
+					the Free Software Foundation, either version 3 of the License, or
+					(at your option) any later version.
+
+					This program is distributed in the hope that it will be useful,
+					but WITHOUT ANY WARRANTY; without even the implied warranty of
+					MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+					GNU General Public License for more details.
+
+					You should have received a copy of the GNU General Public License
+					along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+					USAGE:
+				";
+
+				System.Console.WriteLine(notice.Replace("\t", ""));
+
 				options.WriteOptionDescriptions(System.Console.Out);
 				return;
 			}
